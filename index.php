d0edfa25b315ed2e06bcf48f4bccc92ddd59c483
Mario <mario.skraban@gmail.com>
2019-04-18 12:00:33 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="sl">
+<head>
+    <title>Page Title</title>
+    <link rel="stylesheet" href="vendor/uikit/dist/css/uikit.min.css" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <meta name="keywords" content="your, tags" />
+    <meta name="description" content="150 words" />
+    <meta name="language" content="SL">
+    <meta name="robots" content="index,follow" />
+    <meta name="viewport" content="width=device-width, initial-scale=1"
+
+    <meta name="og:title" content="The Rock" />
+    <meta name="og:type" content="website" />
+    <meta name="og:url" content="http://www.imdb.com/title/tt0117500/" />
+    <meta name="og:image" content="http://ia.media-imdb.com/rock.jpg" />
+    <meta name="og:site_name" content="IMDb" />
+    <meta name="og:description" content="A group of U.S. Marines, under command of..." />
+
+</head>
+<body>
+
+<nav class="uk-navbar-container" uk-navbar>
+    <div class="uk-navbar-left">
+        <ul class="uk-navbar-nav">
+            <li class="uk-active"><a href="">Text 1</a></li>
+            <li class="uk-parent"><a href="">Text 2</a></li>
+            <li><a href=""></a></li>
+        </ul>
+    </div>
+</nav>
+<div class="uk-container">
+    <h1>Naslov 1</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet
+        ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore
+        doloribus!</p>
+</div>
+
+<script src="vendor/jquery/dist/jquery.min.js"></script>
+<script src="vendor/uikit/dist/js/uikit.min.js"></script>
+<script src="vendor/uikit/dist/js/uikit-icons.min.js"></script
+<script src="assets/js/scripts.js"></script
+</body>
+</html>
