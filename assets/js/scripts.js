ac3fba4f0e71dde892af542f69bd83fd713a9655
Mario <mario.skraban@gmail.com>
2019-05-10 14:48:05 +0200

    Test

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,137 @@
+//Spreminjanje pisave
+var size = 18; // Navadno besedilo paragrafi
+var heading1 = 42; // H1
+var heading2 = 36; // H2
+var heading3 = 30; // H3
+var heading4 = 26; // H4
+var heading5 = 22; // H5
+var heading6 = 20; // H6
+var li = 20;
+var form = 20;
 
+$(document).ready(function() {
+
+    fontSize = JSON.parse(localStorage.getItem("fontSize"));
+    console.log(fontSize); //[1, 2, 3]
+    var pSize = fontSize[0];
+    var h1 = fontSize[1];
+    var h2 = fontSize[2];
+    var h3 = fontSize[3];
+    var h4 = fontSize[4];
+    var h5 = fontSize[5];
+    var h6 = fontSize[6];
+    var liSize = fontSize[7];
+    var formSize = fontSize[8];
+
+
+    $('p').css("font-size", pSize + "px");
+
+    $("#big").click(function() {
+        $("h1").css("font-size", heading1 + 2 + "px");
+        $("h2").css("font-size", heading2 + 2 + "px");
+        $("h3").css("font-size", heading3 + 2 + "px");
+        $("h4").css("font-size", heading4 + 2 + "px");
+        $("h5").css("font-size", heading5 + 2 + "px");
+        $("h6").css("font-size", heading6 + 2 + "px");
+        $("p").css("font-size", size + 2 + "px");
+        $("li").css("font-size", size + 2 + "px");
+        $("label").css("font-size", size + 2 + "px");
+        $("form, input, legend, select, textarea, label, a").css("font-size", form + 2 + "px");
+        size++;heading1++;heading2++;heading3++;heading4++;heading5++;heading6++;li++;form++;
+        var fontSize = [size, heading1, heading2, heading3, heading4, heading5, heading6, li, form];
+
+        localStorage.setItem("fontSize", JSON.stringify(fontSize));
+        fontSize = JSON.parse(localStorage.getItem("fontSize"));
+        console.log(fontSize); //[1, 2, 3]
+    });
+    $("#small").click(function() {
+        $("h1").css("font-size", heading1 - 2 + "px");
+        $("h2").css("font-size", heading2 - 2 + "px");
+        $("h3").css("font-size", heading3 - 2 + "px");
+        $("h4").css("font-size", heading4 - 2 + "px");
+        $("h5").css("font-size", heading5 - 2 + "px");
+        $("h6").css("font-size", heading6 - 2 + "px");
+        $("p").css("font-size", size - 2 + "px");
+        $("li").css("font-size", size - 2 + "px");
+        $("label").css("font-size", size - 2 + "px");
+        $("form, input, legend, select, textarea, label, a").css("font-size", form - 2 + "px");
+        size--;heading1--;heading2--;heading3--;heading4--;heading5--;heading6--;li--;form--;
+
+        var fontSize = [size, heading1, heading2, heading3, heading4, heading5, heading6, li, form];
+
+        localStorage.setItem("fontSize", JSON.stringify(fontSize));
+        fontSize = JSON.parse(localStorage.getItem("fontSize"));
+        console.log(fontSize); //[1, 2, 3]
+    });
+
+    //Privzeta CSS tema
+    var tema = "assets/css/main.css";
+    $("head link#tema").attr("href", "assets/css/main.css");
+
+    //Funkcija za preverjanje piskotkov
+    function getCookie(cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for(var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+
+    //CSS teme
+    var css1 = "assets/css/main.css";
+    var css2 = "assets/css/modra.css";
+    var css3 = "assets/css/crna.css";
+    var css4 = "assets/css/kontrast.css";
+
+    var keks=getCookie("tema");
+    if (keks != "") {
+        console.log("Najdena tema:" + keks);
+    }
+    if (keks == "css1") {
+        $("head link#tema").attr("href", css1);
+        console.log("Nastavljena tema 1")
+    }
+    if (keks == "css2") {
+        $("head link#tema").attr("href", css2);
+        console.log("Nastavljena tema 2")
+    }
+    if (keks == "css3") {
+        $("head link#tema").attr("href", css3);
+        console.log("Nastavljena tema 3")
+    }
+    if (keks == "css4") {
+        $("head link#tema").attr("href", css4);
+        console.log("Nastavljena tema 4")
+    }
+
+    $('#cssSelector').change(function(){
+        var izbranaTema = $(this).val();
+        console.log(izbranaTema);
+        if (izbranaTema == 1){
+            $("head link#tema").attr("href", css1);
+            document.cookie = "tema=css1";
+        }
+        else if (izbranaTema == 2){
+            $("head link#tema").attr("href", css2);
+            document.cookie = "tema=css2";
+        }
+        else if (izbranaTema == 3){
+            $("head link#tema").attr("href", css3);
+            document.cookie = "tema=css3";
+        }
+        else if (izbranaTema == 4){
+            $("head link#tema").attr("href", css4);
+            document.cookie = "tema=css4";
+        }
+    });
+
+
+});
--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 <head>
     <title>Page Title</title>
     <link rel="stylesheet" href="vendor/uikit/dist/css/uikit.min.css" />
-    <link rel="stylesheet" href="assets/css/main.css" />
+    <link rel="stylesheet" id="tema" href="assets/css/main.css" />
     <meta name="keywords" content="your, tags" />
     <meta name="description" content="150 words" />
     <meta name="language" content="SL">
     <meta name="robots" content="index,follow" />
-    <meta name="viewport" content="width=device-width, initial-scale=1"
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
     <meta name="og:title" content="The Rock" />
     <meta name="og:type" content="website" />
@@ -16,17 +16,23 @@
     <meta name="og:image" content="http://ia.media-imdb.com/rock.jpg" />
     <meta name="og:site_name" content="IMDb" />
     <meta name="og:description" content="A group of U.S. Marines, under command of..." />
-
 </head>
 <body>
 
 <nav class="uk-navbar-container" uk-navbar>
-    <div class="uk-navbar-left">
-        <ul class="uk-navbar-nav">
-            <li class="uk-active"><a href="">Text 1</a></li>
-            <li class="uk-parent"><a href="">Text 2</a></li>
-            <li><a href=""></a></li>
-        </ul>
+    <div class="uk-container uk-container-xsmall">
+        <select class="uk-select" id="cssSelector">
+            <option value="1">Privzeta tema</option>
+            <option value="2">Modra tema</option>
+            <option value="3">Črna tema</option>
+            <option value="4">Kontrastna tema</option>
+        </select>
+        <div> Velikost pisave
+            <ul class="uk-iconnav">
+                <li><button uk-icon="icon: plus" id="big"></button></li>
+                <li><button uk-icon="icon: minus" id="small"></button></li>
+            </ul>
+        </div>
     </div>
 </nav>
 <div class="uk-container">
@@ -36,9 +42,52 @@
         doloribus!</p>
 </div>
 
+<form>
+    <fieldset class="uk-fieldset">
+
+        <legend class="uk-legend">Legend</legend>
+
+        <div class="uk-margin">
+            <input class="uk-input" type="text" placeholder="Input">
+        </div>
+
+        <div class="uk-margin">
+            <select class="uk-select">
+                <option>Option 01</option>
+                <option>Option 02</option>
+            </select>
+        </div>
+
+        <div class="uk-margin">
+            <textarea class="uk-textarea" rows="5" placeholder="Textarea"></textarea>
+        </div>
+
+        <div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">
+            <label><input class="uk-radio" type="radio" name="radio2" checked> A</label>
+            <label><input class="uk-radio" type="radio" name="radio2"> B</label>
+        </div>
+
+        <div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">
+            <label><input class="uk-checkbox" type="checkbox" checked> A</label>
+            <label><input class="uk-checkbox" type="checkbox"> B</label>
+        </div>
+
+        <div class="uk-margin">
+            <input class="uk-range" type="range" value="2" min="0" max="10" step="0.1">
+        </div>
+
+    </fieldset>
+</form>
+
+<ul class="uk-list">
+    <li>List item 1</li>
+    <li>List item 2</li>
+    <li>List item 3</li>
+</ul>
+
 <script src="vendor/jquery/dist/jquery.min.js"></script>
 <script src="vendor/uikit/dist/js/uikit.min.js"></script>
-<script src="vendor/uikit/dist/js/uikit-icons.min.js"></script
-<script src="assets/js/scripts.js"></script
+<script src="vendor/uikit/dist/js/uikit-icons.min.js"></script>
+<script src="assets/js/scripts.js"></script>
 </body>
 </html>
